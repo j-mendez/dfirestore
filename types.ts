0de164d72de796e41db35cec3e1c81160589d00e
Jeff Mendez <jeffmendez19@gmail.com>
2021-07-03 09:19:32 -0400

    add value type usage

--- types.ts
+++ types.ts
@@ -27,7 +27,7 @@ export interface Value {
   booleanValue: boolean;
   integerValue: string;
   doubleValue: number;
-  timestampValue: string;
+  timestampValue: string | Date;
   stringValue: string;
   bytesValue: string;
   referenceValue: string;
@@ -84,7 +84,9 @@ export interface Write {
 export interface FireRequest {
   collection?: string;
   id?: string;
-  value?: object;
+  value?: {
+    [key: string]: Partial<Value>;
+  };
 }
 
 export interface FetchRequest extends Partial<Request> {
